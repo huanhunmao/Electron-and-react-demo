bf0de7581269f19562c0811c7a00a98857ade282
“fuhaojun” <fuhaojun@52tt.com>
2022-10-13 10:55:38 +0800

    feat:更新 react hooks 部分

--- my-react-review/src/components/ComputeNumberAndTurnButton.jsx
+++ my-react-review/src/components/ComputeNumberAndTurnButton.jsx
@@ -1,8 +1,12 @@
-import React,{ useState } from "react";
+import React,{ useState,useEffect } from "react";
 
 const ComputeNumberAndTurnButton = () => {
     const [number, setNumber] = useState(0);
     const [turn,setTurn] = useState(true)
+
+    useEffect(() => {
+        document.title = `当前数量是${number}`
+    })
 
     const addNumber = () =>{
         setNumber(number + 1)
