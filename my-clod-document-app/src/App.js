a217dd356a87c35eede35c77b2254d70b068186e
“fuhaojun” <fuhaojun@52tt.com>
2022-12-16 09:55:47 +0800

    feat: 新增 TabList

--- my-clod-document-app/src/App.js
+++ my-clod-document-app/src/App.js
@@ -1,22 +1,47 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {faPlus,faFileImport} from '@fortawesome/free-solid-svg-icons'
 import FileSearch from './components/FileSearch';
 import FileList from './components/FileList';
 import defaultFiles from './utils/defaultFiles';
+import BottomBtn from './components/BottomBtn';
+import TabList from './components/TabList';
 
 function App() {
   return (
-    <div className="App container-fluid">
-        <div className="row">
-        <div className='col-3 bg-light  left-panel'>
+    <div className="App container-fluid px-0">
+        <div className="row ">
+        <div className='col-3 bg-light  left-panel no-gutters'>
             <FileSearch title="我的云文档" onFileSearch={(value) => {console.log(value)}}/>
             <FileList files={defaultFiles} 
             onFileClick={() =>{}} 
             onFileDelete={() => {}} 
             onSaveEdit= {(id,newValue) => {console.log('id',id);console.log('newValue',newValue)} }
             />
+            <div className='row no-gutters'>
+                <div className='col'>
+                <BottomBtn 
+                text='新增'
+                colorClass='btn-primary'
+                icon={faPlus}
+                />
+                </div>
+                <div className='col'>
+                <BottomBtn 
+                text='导入'
+                colorClass='btn-success'
+                icon={faFileImport}
+                />
+                </div>
             </div>
-        <h1 className="col-9 bg-primary right-panel">right content</h1>
+            </div>
+        <div className="col-9 right-panel">
+            <TabList
+            files={defaultFiles}
+            onTabClick={(id) => console.log('id',id)}
+            activeId='1'
+            />
+        </div>
         </div>
     </div>
   );
--- my-clod-document-app/src/components/BottomBtn.js
+++ my-clod-document-app/src/components/BottomBtn.js
@@ -4,20 +4,21 @@ import {faPlus} from '@fortawesome/free-solid-svg-icons'
 import PropTypes from 'prop-types';
 
 const BottomBtn = ({text,colorClass,icon,onBtnClick}) => {
-    <button type='button' className={`btn btn-block no-border ${colorClass}`}>
+    return (
+        <button type='button' className={`btn btn-block no-border ${colorClass}`}>
         <FontAwesomeIcon
-        text={faPlus}
         size='lg'
         icon={icon}
         />
         {text}
     </button>
+    )
 }
 
 BottomBtn.propTypes = {
     text:PropTypes.string,
     colorClass:PropTypes.string,
-    icon:PropTypes.string,
+    icon:PropTypes.object.isRequired,
     onBtnClick:PropTypes.func
 }
 
